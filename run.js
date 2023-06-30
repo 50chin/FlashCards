7827c03676b03f76ca9637ec9dc04114921f9a94
50chin <50chin50@gmail.com>
2023-06-30 13:28:52 +0300

    Allo

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 const readlineSync = require('readline-sync');
 const Answer = require('./answer');
 const Question = require('./question');
-const player = require('play-sound')((opts = {}));
+// const player = require('play-sound')((opts = {}));
 
 const an = new Answer();
 const qu = new Question();
@@ -9,14 +9,14 @@ const qu = new Question();
 const userName = readlineSync.question('Напишите свое имя ');
 console.log(`Привет ${userName} !`);
 
-const themes = ['Cartoon', 'Serials', 'EmojiCinema'];
-const iChoose = readlineSync.keyInSelect(themes, 'Выберите тему');
+const themes = ['Cartoon', 'EmojiCinema', 'Serials'];
+const Choose = readlineSync.keyInSelect(themes, 'Выберите тему');
 
 let score = 0;
-for (let i = 0; i < 5; i += 1) {
+for (let i = 0; i < qu.readLineTheme(themes[Choose]).length; i += 1) {
   const user = readlineSync.keyInSelect(
-    an.readLineAnswer(themes[iChoose]),
-    qu.readLineTheme(themes[iChoose])[i]
+    an.readLineAnswer(themes[Choose]),
+    qu.readLineTheme(themes[Choose])[i]
   );
   if (i === user) {
     score += 1;
