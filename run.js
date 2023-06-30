d2508479e50a000e2dd5da97c00c5f9dfee57117
50chin <50chin50@gmail.com>
2023-06-30 12:00:34 +0300

    AddRunJS

--- run.js
+++ run.js
@@ -1,1 +1,17 @@
 const readlineSync = require('readline-sync');
+const player = require('play-sound')(opts = {})
+
+const userName = readlineSync.question('Напишите свое имя ');
+console.log(`Привет ${userName} !`);
+
+const themes = ['Cartoon', 'Serials', 'EmojiCinema'];
+const iChose = readlineSync.keyInSelect(themes, 'Выберите тему');
+
+let count = 0;
+for (let i = 0; i <  ; i += 1) {
+   
+  }
+
+
+
+console.log(`Спасибо за игру!!! Правильных ответов: ${count} из 5`);
